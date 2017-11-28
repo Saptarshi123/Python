f887cb3e3137027a1981df91d3cfe2f9a7a2d711
Saha <saptarshi.saha@capgemini.com>
2017-11-28 13:21:34 +0530

    adding lab3

---
+++ Lab3.py
@@ -0,0 +1,10 @@
+num=1
+for num in range(1,50):
+    if(num%3==0):
+        print("Fizz")
+    elif(num%5==0):
+        print("Buzz")
+    elif(num%3==0 and num%5==0):
+        print("FizzBuzz")
+    else:
+        print(num)
