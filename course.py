1b34bb30f22b2a51c74c70dc181d1c1a1bf0612d
Saptarshi123 <saptarshisaha123@gmail.com>
2017-11-30 16:54:33 +0530

    Delete course.py

--- course.py
+++
@@ -1,8 +0,0 @@
-class Course:
-
-    def getCourseName(self):
-        return self.courseName
-    def setCourseName(self,courseName):
-        self.courseName=courseName
-    def __repr__(self):
-     return self.courseName
